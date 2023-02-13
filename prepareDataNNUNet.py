4f076677bec363bc15a53f0d7189263c21927aae
Kate Cevora <katecevora@kates-mbp.home>
2023-02-13 15:09:25 +0000

    added try/except clause

--- prepareDataNNUNet.py
+++ prepareDataNNUNet.py
@@ -16,37 +16,41 @@ def prep2DData():
 
     for f in folders:
         if f.startswith("pancreas"):
-            print(f)
+            try:
+                print(f)
 
-            # open label and image
-            img_nii = nib.load(os.path.join(data_dir, f, f + "_warp.nii.gz"))
-            gt_nii = nib.load(os.path.join(data_dir, f, f + "_label_warp.nii.gz"))
+                # open label and image
+                img_nii = nib.load(os.path.join(data_dir, f, f + "_warp.nii.gz"))
+                gt_nii = nib.load(os.path.join(data_dir, f, f + "_label_warp.nii.gz"))
 
-            gt = gt_nii.get_fdata()
+                gt = gt_nii.get_fdata()
 
-            # flatten label and find extent
-            gt_flat = np.sum(gt, axis=(0, 1))
-            indicies = np.where(gt_flat > 0)[0]
+                # flatten label and find extent
+                gt_flat = np.sum(gt, axis=(0, 1))
+                indicies = np.where(gt_flat > 0)[0]
 
-            # Find where extent of pancreas slice is greatest
-            x = np.argmax(gt_flat)
+                # Find where extent of pancreas slice is greatest
+                x = np.argmax(gt_flat)
 
-            # save the slices around the middle (along wih labels)
-            for i in range(3):
-                slice_idx = x - i
-                gt_slice = gt_nii.slicer[:, :, slice_idx:slice_idx + 1]
-                img_slice = img_nii.slicer[:, :, slice_idx:slice_idx + 1]
+                # save the slices around the middle (along wih labels)
+                for i in range(3):
+                    slice_idx = x - i
+                    gt_slice = gt_nii.slicer[:, :, slice_idx:slice_idx + 1]
+                    img_slice = img_nii.slicer[:, :, slice_idx:slice_idx + 1]
 
-                nib.save(gt_slice, os.path.join(lab_dir, f + "{0}.nii.gz".format(slice_idx)))
-                nib.save(img_slice, os.path.join(img_dir, f + "{0}_0000.nii.gz".format(slice_idx)))
+                    nib.save(gt_slice, os.path.join(lab_dir, f + "{0}.nii.gz".format(slice_idx)))
+                    nib.save(img_slice, os.path.join(img_dir, f + "{0}_0000.nii.gz".format(slice_idx)))
 
-            for i in range(1, 3):
-                slice_idx = x + i
-                gt_slice = gt_nii.slicer[:, :, slice_idx:slice_idx + 1]
-                img_slice = img_nii.slicer[:, :, slice_idx:slice_idx + 1]
+                for i in range(1, 3):
+                    slice_idx = x + i
+                    gt_slice = gt_nii.slicer[:, :, slice_idx:slice_idx + 1]
+                    img_slice = img_nii.slicer[:, :, slice_idx:slice_idx + 1]
 
-                nib.save(gt_slice, os.path.join(lab_dir, f + "{0}.nii.gz".format(slice_idx)))
-                nib.save(img_slice, os.path.join(img_dir, f + "{0}_0000.nii.gz".format(slice_idx)))
+                    nib.save(gt_slice, os.path.join(lab_dir, f + "{0}.nii.gz".format(slice_idx)))
+                    nib.save(img_slice, os.path.join(img_dir, f + "{0}_0000.nii.gz".format(slice_idx)))
+            except:
+                print(f + " failed")
+                continue
 
 
 def main():
