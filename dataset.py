bfda6bf0b355f164ba964733bfd2c4a13b240d3d
Kate Cevora <katecevora@kates-mbp.home>
2023-04-05 15:14:51 +0100

    Added some error catching

--- dataset.py
+++ dataset.py
@@ -48,28 +48,48 @@ def samplePatchWeighted(image, label, probs, patch_size):
     c = np.argmax(np.random.multinomial(1, probs))
 
     # Choose the relevant part of the label map, convert to verticies and crop so that patch is within image limits
-    # TODO (check this will work for pancreas)
     verts = np.nonzero(label[c, :, :] == c)
 
-    cx = verts[:, 0]
-    cy = verts[:, 1]
+    cx = np.unique(verts[:, 0])
+    cy = np.unique(verts[:, 1])
 
-    vert_min = int(np.ceil(patch_size / 2))
-    vert_max = image.shape[1] - int(np.floor(patch_size / 2))
+    delta = int(np.floor(patch_size / 2))
+    vert_min = delta
+    vert_max = image.shape[1] - delta
 
-    cx_cropped = cx[(cx > vert_min)]
-    cx_cropped = cx[(cx < vert_max)]
-    cy_cropped = cy[(cy > vert_min)]
-    cy_cropped = cy[(cy < vert_max)]
+    cx_cropped = cx[(cx > vert_min) & (cx < vert_max)]
+    cy_cropped = cy[(cy > vert_min) & (cy < vert_max)]
 
-    # Randomly sample to get the central verticies of the patch
-    x = int(np.random.choice(cx_cropped, size=1))
-    y = int(np.random.choice(cy_cropped, size=1))
+    # Check if this has resulted in no suitable vertices
+    if (cy_cropped.shape[0] < 1) or (cx_cropped.shape[0] < 1):
+        y = 256
+        x = 256
+    else:
+        # Randomly sample to get the central verticies of the patch
+        x = int(np.random.choice(cx_cropped, size=1))
+        y = int(np.random.choice(cy_cropped, size=1))
 
     # Crop the patch from the image and label
-    delta = int(np.floor(patch_size / 2))
     image_cropped = image[:, x-delta:x+delta, y-delta:y+delta]
     label_cropped = label[:, x-delta:x+delta, y-delta:y+delta]
+
+    if np.min(cx_cropped) - delta < 0:
+        print(cx_cropped)
+        raise(Exception("x coordinate is less than minimum"))
+    if np.min(cy_cropped) - delta < 0:
+        print(cy_cropped)
+        raise(Exception("y coordinate is less than minimum"))
+    if np.max(cx_cropped) + delta > 512:
+        print(cx_cropped)
+        raise (Exception("x coordinate is greater than maximum"))
+    if np.max(cy_cropped) + delta > 512:
+        print(cy_cropped)
+        raise (Exception("y coordinate is greater than maximum"))
+
+    if image_cropped.shape[2] < 256:
+        print(image_cropped.shape)
+        print(x), print(y)
+        print(cy_cropped), print(cx_cropped)
 
     return image_cropped, label_cropped
 
