6593f49dec31d20079e659fe020b87da3940a6cd
Kate Cevora <katecevora@kates-mbp.home>
2023-04-14 15:17:35 +0100

    added switches for all transforms

--- transforms.py
+++ transforms.py
@@ -10,17 +10,6 @@ import matplotlib.pyplot as plt
 
 import nibabel as nib
 
-# CT data has Gaussian distributed noise
-class AddGaussianNoise(object):
-
-    def __init__(self, sigma=0.1):
-        self.sigma = sigma
-
-    def __call__(self, data):
-        noisy_image = data + np.random.normal(loc=0.0, scale=self.sigma, size=data.shape)
-
-        return noisy_image
-
 
 def clipAndNormalise(image, mu, sigma, percentiles):
     """
@@ -68,78 +57,107 @@ def clipAndNormalise(image, mu, sigma, percentiles):
     return img_raw_normed
 
 
-def create_elastic_deformation(image_shape, num_controlpoints, sigma):
-    # Create an instance of a SimpleITK image of the same size as our image
-    itkimg = sitk.GetImageFromArray(np.zeros(image_shape))
+def randomAffine(subject, probability=0.2):
+    affine_transform = tio.RandomAffine(scales=(0.7, 1.4),
+                                        degrees=(-180, 180),
+                                        isotropic=True,
+                                        label_keys="label",
+                                        include=["image", "label"],
+                                        p=probability)
+    subject = affine_transform(subject)
 
-    # This parameter is just a list with the number of control points per image dimensions
-    trans_from_domain_mesh_size = [num_controlpoints] * itkimg.GetDimension()
-
-    # We initialise the transform here: Passing the image size and the control point specifications
-    bspline_transformation = sitk.BSplineTransformInitializer(itkimg, trans_from_domain_mesh_size)
-
-    # Isolate the transform parameters: the bspline control points and spline coefficients
-    params = np.asarray(bspline_transformation.GetParameters(), dtype=float)
-
-    # Let's initialise the transform by randomly displacing each control point by a random distance (magnitude sigma)
-    params = np.random.randn(params.shape[0]) * sigma
-    bspline_transformation.SetParameters(tuple(params))
-
-    return bspline_transformation
+    return subject
 
 
-class ApplyElasticDeformation(object):
+def randomGaussianNoise(subject, probability=0.15):
+    noise = tio.RandomNoise(p=probability, std=(0, 0.1), include=["image"])
+    subject = noise(subject)
 
-    def __init__(self, sigma, num_control_points):
-        # strength of transformation
-        self.sigma = sigma
-
-        # granularity of transformation
-        self.num_control_points = num_control_points
-
-    def __call__(self, image, label):
-        # We need to choose an interpolation method for our transformed image, let's just go with b-spline
-        resampler = sitk.ResampleImageFilter()
-        resampler.SetInterpolator(sitk.sitkBSpline)
-
-        # create empty container for output image
-        warped_img = np.zeros(image.shape)
-
-        # transform each channel with the same deformation
-        # we are trying to simulate variations in brain structure here
-        n_channels = image.shape[0]
-        image_shape = image[0, :, :].shape
-
-        # Initialise the transform
-        bspline_transform = create_elastic_deformation(image_shape, self.num_control_points, self.sigma)
-
-        for i in range(n_channels):
-            # Let's convert our image to an sitk image
-            single_channel_img = image[i, :, :]
-            sitk_image = sitk.GetImageFromArray(single_channel_img)
-            # sitk_grid = create_grid(image)
-
-            # Specify the image to be transformed: This is the reference image
-            resampler.SetReferenceImage(sitk_image)
-            resampler.SetDefaultPixelValue(0)
-
-            # Set the transform in the initialiser
-            resampler.SetTransform(bspline_transform)
-
-            # Carry out the resampling according to the transform and the resampling method
-            out_img_sitk = resampler.Execute(sitk_image)
-            # out_grid_sitk = resampler.Execute(sitk_grid)
-
-            # Convert the image back into a python array
-            out_img = sitk.GetArrayFromImage(out_img_sitk)
-
-            # store channel in output container
-            warped_img[i, :, :] = out_img.reshape(single_channel_img.shape)
-
-        return warped_img
+    return subject
 
 
-# Compose the image transforms for our dataset
+def randomGaussianBlur(subject, probability=0.1):
+    # width of kernel is in mm
+    blur = tio.RandomBlur(p=0.1, std=(1.25, 3.75), include=["image"])
+    subject = blur(subject)
+
+    return subject
+
+
+def randomBrightnessMul(subject, probability=0.15):
+    n = np.random.uniform()
+    if n < probability:
+        # Randomly select the brightness multiplier
+        m = np.random.uniform(0.7, 1.3)
+        min = torch.min(subject["image"]).detach().numpy()
+        max = torch.max(subject["image"]).detach().numpy()
+
+        scale = tio.RescaleIntensity(out_min_max=(m * min, m * max), include=["image"])
+        subject = scale(subject)
+
+    return subject
+
+
+def randomContrastMod(subject, probability=0.15):
+    n = np.random.uniform()
+    if n < probability:
+        # Randomly select the brightness multiplier
+        m = np.random.uniform(0.7, 1.3)
+        min = torch.min(subject["image"])
+        max = torch.max(subject["image"])
+
+        scale = tio.RescaleIntensity(out_min_max=(m * min.detach().numpy(), m * max.detach().numpy()),
+                                     include=["image"])
+        subject = scale(subject)
+
+        # Clip back to original value range
+        clamp = tio.Clamp(out_min=min, out_max=max, include=["image"])
+        subject = clamp(subject)
+
+    return subject
+
+
+def lowResSim(subject, probability=0.125):
+    n = np.random.uniform()
+    if n < probability:
+        # first downsample, then upsample
+        downsample = tio.Resize(target_shape=(256, 256, 1), include=["image", "label"])
+        upsample = tio.Resize(target_shape=(512, 512, 1), include=["image", "label"])
+
+        subject = downsample(subject)
+        subject = upsample(subject)
+
+    return subject
+
+
+def randomGamma(subject, probability=0.15):
+    n = np.random.uniform()
+    if n < probability:
+        # need to scale intensities to range [0,1] first, and then back after transform
+        # find initial min/max
+        min = np.min(subject["image"].detach().numpy())
+        max = np.max(subject["image"].detach().numpy())
+
+        # set up operations
+        scalePre = tio.RescaleIntensity(out_min_max=(0, 1), include=["image"])
+        gamma = tio.RandomGamma(p=1, log_gamma=(0.7, 1.5), include=["image"])
+        scalePost = tio.RescaleIntensity(out_min_max=(min, max), include=["image"])
+
+        # Apply to sample
+        subject = scalePre(subject)
+        subject = gamma(subject)
+        subject = scalePost(subject)
+
+    return subject
+
+
+def randomFlip(subject, probability=0.5):
+    flip = tio.RandomFlip(axes=(0, 1), flip_probability=probability, include=["image", "label"])
+    subject = flip(subject)
+
+    return subject
+
+
 def augmentImage(image, label):
     """
     Image and label should have 4 dimensions each (C, H, W, D)
@@ -155,84 +173,43 @@ def augmentImage(image, label):
     # Create a dictionary to feed to transform
     subject = {"image": image, "label": label}
 
+    #augmentation_list = ['affine', 'gaussian_noise', "gaussian_blur", "brightness", "contrast", "low-res", "gamma", "flip"]
+    augmentation_list = ["affine"]
+
     # AFFINE TRANSFORMATION
-    affine_transform = tio.RandomAffine(scales=(0.7, 1.4),
-                                        degrees=(-180, 180),
-                                        isotropic=True,
-                                        label_keys="label",
-                                        include=["image", "label"],
-                                        p=0.2)
-    transformed_subject = affine_transform(subject)
+    if 'affine' in augmentation_list:
+        subject = randomAffine(subject, probability=0.2)
 
     # GAUSSIAN NOISE
-    noise = tio.RandomNoise(p=0.15, std=(0, 0.1), include=["image"])
-    transformed_subject = noise(transformed_subject)
+    if 'gaussian_noise' in augmentation_list:
+        subject = randomGaussianNoise(subject, probability=0.15)
 
-    # GAUSSIAN BLUR (width of kernel is in mm)
-    blur = tio.RandomBlur(p=0.1, std=(1.25, 3.75), include=["image"])
-    transformed_subject = blur(transformed_subject)
+    # GAUSSIAN BLUR
+    if "gaussian_blur" in augmentation_list:
+        subject = randomGaussianBlur(subject, probability=0.1)
 
     # BRIGHTNESS MODIFICATION
-    n = np.random.uniform()
-    if n < 0.15:
-        # Randomly select the brightness multiplier
-        m = np.random.uniform(0.7, 1.3)
-        min = torch.min(transformed_subject["image"]).detach().numpy()
-        max = torch.max(transformed_subject["image"]).detach().numpy()
-
-        scale = tio.RescaleIntensity(out_min_max=(m * min, m * max), include=["image"])
-        transformed_subject = scale(transformed_subject)
+    if "brightness" in augmentation_list:
+        subject = randomBrightnessMul(subject, probability=0.15)
 
     # CONTRAST MODIFICATION
-    n = np.random.uniform()
-    if n < 0.15:
-        # Randomly select the brightness multiplier
-        m = np.random.uniform(0.7, 1.3)
-        min = torch.min(transformed_subject["image"])
-        max = torch.max(transformed_subject["image"])
-
-        scale = tio.RescaleIntensity(out_min_max=(m * min.detach().numpy(), m * max.detach().numpy()), include=["image"])
-        transformed_subject = scale(transformed_subject)
-
-        # Clip back to original value range
-        clamp = tio.Clamp(out_min=min, out_max=max, include=["image"])
-        transformed_subject = clamp(transformed_subject)
+    if "contrast" in augmentation_list:
+        subject = randomContrastMod(subject, probability=0.15)
 
     # LOW-RES SIMULATION
-    n = np.random.uniform()
-    if n < 0.125:
-        # first downsample, then upsample
-        downsample = tio.Resize(target_shape=(256, 256, 1), include=["image", "label"])
-        upsample = tio.Resize(target_shape=(512, 512, 1), include=["image", "label"])
-
-        transformed_subject = downsample(transformed_subject)
-        transformed_subject = upsample(transformed_subject)
-
+    if "low_res" in augmentation_list:
+        subject = lowResSim(subject, probability=0.125)
 
     # GAMMA AUGMENTATION
-    n = np.random.uniform()
-    if n < 0.15:
-        # need to scale intensities to range [0,1] first, and then back after transform
-        # find initial min/max
-        min = np.min(transformed_subject["image"].detach().numpy())
-        max = np.max(transformed_subject["image"].detach().numpy())
-
-        # set up operations
-        scalePre = tio.RescaleIntensity(out_min_max=(0, 1), include=["image"])
-        gamma = tio.RandomGamma(p=1, log_gamma=(0.7, 1.5), include=["image"])
-        scalePost = tio.RescaleIntensity(out_min_max=(min, max), include=["image"])
-
-        # Apply to sample
-        transformed_subject = scalePre(transformed_subject)
-        transformed_subject = gamma(transformed_subject)
-        transformed_subject = scalePost(transformed_subject)
+    if "gamma" in augmentation_list:
+        subject = randomGamma(subject, probability=0.15)
 
     # MIRRORING (RANDOM FLIP)
-    flip = tio.RandomFlip(axes=(0, 1), flip_probability=0.5, include=["image", "label"])
-    transformed_subject = flip(transformed_subject)
+    if "flip" in augmentation_list:
+        subject = randomFlip(subject, probability=0.5)
 
-    transformed_img = transformed_subject["image"]
-    transformed_lab = transformed_subject["label"]
+    transformed_img = subject["image"]
+    transformed_lab = subject["label"]
 
     # squeeze before returning
     transformed_img = transformed_img.squeeze(0)
@@ -263,7 +240,7 @@ def main():
         transformed_img, transformed_lab = augmentImage(img_tensor, lab_tensor)
 
         # plot both
-        if True:
+        if False:
             plt.clf()
             plt.subplot(2, 2, 1)
             plt.imshow(img_tensor[:, :, 0], cmap='gray')
